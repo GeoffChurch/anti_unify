81f81e1defe271f681d9858ed79f10c201a5b135
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2023-01-15 21:49:23 -0500

    Restructured to conform to SWI pack style

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(anti_unify).
+version('0.1').
+title('Anti-unification. anti_unify/3 is intended as a relational drop-in replacement for term_subsumer/3.').
+author('Geoffrey Churchill', 'geoffrey.a.churchill@gmail.com').
+home('https://github.com/GeoffChurch/anti_unify').
+requires(subsumes).
