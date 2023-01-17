4de0f30aab2034c465b6308026e845d70e572fb4
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2023-01-16 23:14:57 -0500

    Bump version 0.1 => 0.2

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(anti_unify).
-version('0.1').
+version('0.2').
 title('Anti-unification. anti_unify/3 is intended as a relational drop-in replacement for term_subsumer/3.').
 author('Geoffrey Churchill', 'geoffrey.a.churchill@gmail.com').
 home('https://github.com/GeoffChurch/anti_unify').
